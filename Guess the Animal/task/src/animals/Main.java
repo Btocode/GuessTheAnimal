b4e55762026e67ca5fb6065ff4c893a734de42ab
Afsan Saeed <afsansaeed2017@gmail.com>
2022-05-29 18:15:00 +0600

    stage 2 completed

--- Guess the Animal/task/src/animals/Main.java
+++ Guess the Animal/task/src/animals/Main.java
@@ -6,6 +6,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         LocalTime now = LocalTime.now();
         int time = now.getHour();
         String greeting;
@@ -18,34 +19,35 @@ public class Main {
         } else {
             greeting = "Good Evening!";
         }
-        System.out.println(greeting + " \n\nEnter an Animal:");
 
 //        Taking inputs
         System.out.println(greeting + "\n");
         System.out.println("Enter the first Animal:");
-        String[] firstAnimalArray = sc.nextLine().toLowerCase().split(" ");
+        String[] firstAnimalArray = sc.nextLine().toLowerCase().trim().split(" ");
         System.out.println("Enter the Second Animal:");
-        String[] secondAnimalArray = sc.nextLine().toLowerCase().split(" ");
+        String[] secondAnimalArray = sc.nextLine().toLowerCase().trim().split(" ");
 
         Animal firstAnimalObj = getPropertiesForAnimal(firstAnimalArray);
         Animal secondAnimalObj = getPropertiesForAnimal(secondAnimalArray);
 
-        System.out.println("Specify a fact that distinguishes "+ firstAnimalObj.getArticle() + " " +firstAnimalObj.getName()+ " from " + secondAnimalObj.getArticle() + " " +secondAnimalObj.getName()+".\nThe sentence should be of the format: 'It can/has/is ...'.");
+        System.out.println("Specify a fact that distinguishes " + firstAnimalObj.getArticle() + " " + firstAnimalObj.getName() + " from " + secondAnimalObj.getArticle() + " " + secondAnimalObj.getName() + ".\nThe sentence should be of the format: 'It can/has/is ...'.");
 
         //Taking input for animal property
-        String[] statement;
-        while(sc.hasNext()){
-            statement = sc.nextLine().split(" ");
-            Boolean isCorrectFormat = verifyStatement(statement);
+        String statementGiven = "";
+        while (sc.hasNext()) {
+            statementGiven = sc.nextLine().trim();
+            String[] statementArray = statementGiven.split(" ");
+            Boolean isCorrectFormat = verifyStatement(statementArray);
 
-            if(isCorrectFormat){
+            if (isCorrectFormat) {
                 break;
-            }
-            else{
+            } else {
                 System.out.println("The examples of a statement:\n" +
                         " - It can fly\n" +
                         " - It has horn\n" +
                         " - It is a mammal");
+                System.out.println("Specify a fact that distinguishes " + firstAnimalObj.getArticle() + " " + firstAnimalObj.getName() + " from " + secondAnimalObj.getArticle() + " " + secondAnimalObj.getName() + ".\nThe sentence should be of the format: 'It can/has/is ...'.");
+
             }
         }
 
@@ -54,68 +56,128 @@ public class Main {
         String responseText = sc.nextLine().toLowerCase().trim();
 
 //            Based on the user response print something
-        if(responseText.equals("yes")){
-            System.out.println("The " + firstAnimalObj.getName() + " "+ );
+        String[] statement = statementGiven.split(" ");
+        StringBuilder tempString = new StringBuilder();
+        StringBuilder tempString1 = new StringBuilder();
+        if (responseText.equals("yes")) {
+            //statement is true for animal 2
+
+            tempString.append("The ")
+                    .append(firstAnimalObj.getName())
+                    .append(" ").append(statement[1].contains("has") ? "does" : statement[1])
+                    .append(statement[1].contains("can") ? "'t " : statement[1].contains("has") ? "n't have ": "n't ");
+            sentenceMaker(statement, tempString);
+
+            System.out.println("I learned the following facts about animals:\n-"
+                    + tempString.toString().trim());
+
+            tempString1.append("The ")
+                    .append(secondAnimalObj.getName())
+                    .append(" ").append(statement[1])
+                    .append(" ");
+        } else {
+            //statement is true for animal 2
+
+            tempString.append("The ")
+                    .append(firstAnimalObj.getName())
+                    .append(" ").append(statement[1]).append(" ");
+            sentenceMaker(statement, tempString);
+
+            System.out.println("I learned the following facts about animals:\n-"
+                    + tempString.toString().trim());
+
+            tempString1.append("The ")
+                    .append(secondAnimalObj.getName())
+                    .append(" ").append(statement[1].contains("has") ? "does" : statement[1])
+                    .append(statement[1].contains("can") ? "'t " : statement[1].contains("has") ? "n't have ": "n't ");
+            sentenceMaker(statement, tempString);
         }
-        else{
-            StringBuilder tempString = new StringBuilder();
-            for (int i = 2; i < statement.length; i++) {
-                tempString.append(statement[i]).append(" ");
-            }
-            System.out.println();
-        }
+        printStatement(statement, tempString1);
 
 
     }
 
-    public static Boolean verifyStatement(String[] statement){
-        if(statement.length > 1){
-            return (statement[0].compareTo("It") == 0) && statement[1].contains("can") || statement[1].contains("has") || statement[1].contains("is");
+    private static void printStatement(String[] statement, StringBuilder tempString1) {
+        sentenceMaker(statement, tempString1);
+        System.out.print("-"
+                + tempString1.toString().trim() + "\n");
+
+
+//            Question Generator
+        StringBuilder tempString2 = new StringBuilder();
+
+        if (statement[1].equals("has")) {
+            tempString2.append("Does").append(" ").append(statement[0].toLowerCase(Locale.ROOT)).append(" ").append("have").append(" ");
+            appendStringArray(tempString2, statement);
+        } else {
+            String firstWord = statement[1].substring(0, 1).toUpperCase() + statement[1].substring(1);
+            tempString2.append(firstWord).append(" ").append(statement[0].toLowerCase(Locale.ROOT)).append(" ");
+            appendStringArray(tempString2, statement);
         }
-        else return false;
+
+        System.out.println("I can distinguish these animals by asking the question:\n-" + tempString2.toString().trim());
+    }
+
+    private static void sentenceMaker(String[] statement, StringBuilder tempString1) {
+        for (int i = 2; i < statement.length - 1; i++) {
+            tempString1.append(statement[i]).append(" ");
+        }
+//        tempString1.append(statement[statement.length-1].contains(".") ? statement[statement.length-1] : statement[statement.length-1] + ".");
+        tempString1.append(statement[statement.length - 1].contains("?") ?
+                statement[statement.length - 1].replace("?", ".") :
+                statement[statement.length - 1].contains(".") ?
+                        statement[statement.length - 1] : statement[statement.length - 1] + ".");
+    }
+
+    private static void appendStringArray(StringBuilder tempString, String[] statement) {
+        for (int i = 2; i < statement.length - 1; i++) {
+            tempString.append(statement[i]).append(" ");
+        }
+        tempString.append((statement[statement.length - 1]).replace(".", "?"));
+//        tempString.append("");
+    }
+
+    public static Boolean verifyStatement(String[] statement) {
+        if (statement.length > 1) {
+            return (statement[0].compareTo("It") == 0) && (statement[1].contains("can") || statement[1].contains("has") || statement[1].contains("is"));
+
+        } else return false;
 
     }
-    public static Animal getPropertiesForAnimal(String[] inputArray){
+
+    public static Animal getPropertiesForAnimal(String[] inputArray) {
         Animal animal = new Animal();
         String article;
         StringBuilder animalContainer = new StringBuilder();
         if (inputArray[0].equals("the")) {
             article = assignAnArticle(inputArray[1]);
-            for (int i = 1; i< inputArray.length ; i++) {
+            for (int i = 1; i < inputArray.length; i++) {
                 animalContainer.append(inputArray[i]).append(" ");
 
-                    }
-                   if(punctCount < 2){
-                       confirmation = confirmation.replaceAll("\\.|\\!","");
-                   }
-                   //do nothing
-
-                }
-
-                if (positiveConfirmations.contains(confirmation)) {
-                    System.out.println("You answered: Yes");
-                    break;
-                } else if (negativeConfirmation.contains(confirmation)) {
-                    System.out.println("You answered: No");
-                    break;
-                } else {
-                    int randomIndex = (int) ((Math.random() * (clarificationQuestions.size())) + 0);
-                    System.out.println(clarificationQuestions.get(randomIndex));
-                }
             }
-//        Generating random goodbye text for user
-            int randomIndex = (int) ((Math.random() * (goodByeList.size())) + 0);
-            System.out.println("\n" + goodByeList.get(randomIndex) + "!");
-            sc.close();
+        } else if (inputArray[0].equals("a") || inputArray[0].equals("an")) {
+            article = inputArray[0];
+            for (int i = 1; i < inputArray.length; i++) {
+                animalContainer.append(inputArray[i]).append(" ");
 
+            }
+        } else {
+            article = assignAnArticle(inputArray[0]);
+            for (String s : inputArray) {
+                animalContainer.append(s).append(" ");
+            }
         }
-        //    Assigning article for animals
-        private static String assignAnArticle (String animal){
-            char alphabet = animal.charAt(0);
-            if (alphabet == 'a' || alphabet == 'e' || alphabet == 'i' || alphabet == 'o' || alphabet == 'u') {
-                return "an";
-            } else {
-                return "a";
-            }
+        animal.setArticle(article);
+        animal.setName(animalContainer.toString().trim());
+        return animal;
+    }
+
+    private static String assignAnArticle(String animal) {
+        char alphabet = animal.charAt(0);
+        if (alphabet == 'a' || alphabet == 'e' || alphabet == 'i' || alphabet == 'o' || alphabet == 'u') {
+            return "an";
+        } else {
+            return "a";
         }
     }
+}
